4004aa0546926a45a3acc1b2e104df15b0772f0e
Pahansathsara360 <Pahansathsara360@gmail.com>
2024-12-10 18:54:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SzpAlJxQ#OtG9YbE_b7Dl7cSXwl81beWSKrCRxJy7IPHG-OCgD8U", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "jmZXTRQY#x_k-4uYAYRG55Jp1J0su97Z26-vvuZHDgz-EA-qgoww", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
